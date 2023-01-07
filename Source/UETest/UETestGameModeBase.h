6b0b21d668be600ec44e86bab51481949beccdcd
awei-mj <2510558223@qq.com>
2023-01-07 19:40:30 +0800

    initialization
    初始化

---
+++ Source/UETest/UETestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UETestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UETEST_API AUETestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
